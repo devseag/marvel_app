4c9c3eae9c2f6a3ed554a56f2bf06ffe503ffa1c
devseag <noexample@gmail.com>
2023-09-23 19:04:11 -0300

    add batching flush sync
    8 171

---
+++ src/components/testComponent/TestComponent.js
@@ -0,0 +1,33 @@
+import { useState, flushSync } from "react";
+
+function TestComponent() {
+    const [count, setCount] = useState(0);
+    const [flag, setFlag] = useState(false);
+
+    function handleClick() {
+        // setCount( c => c + 1);
+        // setFlag(f => !f);
+        setTimeout(() => {
+            // setCount(c => c + 1);
+            // setFlag(f => !f);
+            flushSync(() => {
+                setCount(c => c + 1);
+            })
+                
+            flushSync(() => {
+                setFlag(f => !f);             
+            })
+        }, 100);
+    }
+
+    console.log('render');
+
+    return (
+        <div>
+            <button onClick={handleClick}>Next</button>
+            <h1 style={{ color: flag ? "blue" : "black"}}>{count}</h1>
+        </div>
+    )
+}
+
+export default TestComponent;
